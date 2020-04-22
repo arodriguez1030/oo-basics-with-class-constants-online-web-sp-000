e9285dff47c886014f3aec0f28e3bec441d471bf
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-04-22 18:55:42 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,8 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
 
-GENRES = []
+GENRES = ["Thriller", "Science Fiction", "Romance"]
   def initialize(title)
     @title = title
   end
@@ -10,5 +10,9 @@ GENRES = []
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-
+  
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
 end
